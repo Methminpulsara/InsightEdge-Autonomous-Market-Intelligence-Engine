6b8b77cc8bd10fb4a2e83e360103463ac5ae8fce
Methminpulsara <methminpulsara10@gmail.com>
2026-02-01 00:34:50 +0530

    add research agent for system

---
+++ agents/research_agent.py
@@ -0,0 +1,28 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from tools.search_tool import get_search_tool
+from core.state import AgentState
+
+
+def research_agent(state: AgentState):
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    search = get_search_tool()
+
+    # search
+    query = f"Current market trends for:  {state:['business_idea']}"
+    search_result = search.invoke(query)
+
+    # promt template
+    prompt_template = ChatPromptTemplate.from_messages([
+        ("system",
+         "You are an expert Market Researcher. Use the provided search results to summarize demand, trends, and audience."),
+        ("human", "Business Idea: {idea}\n\nSearch Results: {results}")
+    ])
+
+    chain = prompt_template | llm
+
+    response = chain.invoke({"idea": state['business_idea'],
+                             "result": search_result
+                             })
+
+    return {"market_research_data": response.content}
