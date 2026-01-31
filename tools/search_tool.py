9e5daaa67f1d02722ba2dad40cf337353e076225
Methminpulsara <methminpulsara10@gmail.com>
2026-02-01 00:13:27 +0530

    add main tool for search things on google

---
+++ tools/search_tool.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+load_dotenv()
+
+def get_search_tool():
+    # for search in Google / Internet
+    api_wrapper = TavilySearchAPIWrapper()
+    return TavilySearchResults(api_wrapper=api_wrapper, max_results=5)
